4631f2f4aefa67fab65b55d7bb7477c887d85385
quazzzar <abashuiasa@gmail.com>
2016-10-14 16:29:19 +0300

    modificari noi

--- sender.js
+++ sender.js
@@ -8,14 +8,25 @@ client.connect(PORT, HOST, function() {
 
     console.log('CONECTAT LA: ' + HOST + ':' + PORT);
     // Scrie un mesaj catre socket la conectare, serverul primeste mesajul de la client 
-    	setInterval(function(){ client.write('Mesajul nr. ' + i); i++ }, 1000);
+    	// setInterval(function(){ 
+    	// 	client.write('Mesajul nr. ' + i); i++;
+    	// }, 1000);
+
+    	client.write('Mesajul nr. 1\n');
+    	client.write('Mesajul nr. 2\n');
+    	client.write('Mesajul nr. 3\n');
+    	client.write('Mesajul nr. 4\n');
+    	client.write('Mesajul nr. 5\n');
 	});
 
-	// Adaugam un handler la pt evenimentul 'data'
+	// Adaugam un handler pt evenimentul 'data'
 	// data este ceea ce serverul transmite catre socket
 	client.on('data', function(data) {
-	    console.log('Message: ' + data);
+	    console.log(data.toString());
 	});
+
+	client.on('error', function(){
+  });
 
 //Adaugam un handler pentru evenimentul 'close'
 client.on('close', function() {
--- server.js
+++ server.js
@@ -1,29 +1,62 @@
 var net = require('net');
-
+var fs = require('fs');
 var HOST = '127.0.0.1';
 var PORT = 3000;
+
+// in acest array salvam fiecare client
+var clients = [];
 
 // Cream o instanta de server si legam portul de ea.
 // Functia din net.CreateServer() devine handler pentru evenimentul 'connection'
 // Obiectul socket devine UNIC pentru fiecare conexiune.
 net.createServer(function(socket) {
-    
+
+    //il identificam pe fiecare
+	socket.name = "User:" + socket.remotePort;
+    //si dupa asta il "impingem" in array-ul clients
+    clients.push(socket);
+    console.log("S-a mai conectat un User. Total useri: " + clients.length);
+    //la conectare ii afisam un mesaj de intampinare
+	socket.write("Salut " + socket.name + "\n");
+	
     // Avem o conexiune - un obiect socket este atribuit ei automat.
-    console.log('S-a conectat: ' + socket.remoteAddress +':'+ socket.remotePort);
+    broadcast(socket.name + " joined the chat\n", socket);
     
     // Adaugam un handler pentru 'data' event al aceastei instante de socket
     socket.on('data', function(data) {
-        
-        console.log('Clientul ' + socket.remoteAddress + ': ' + data);
-        // scriem date pentru socket, clientul va primi datele ca date de la server
-        socket.write('"' + data + '"');
-        
-    });
     
-    // Adaugam un hadler pentru evenimentul 'close' al fiecarui socket
-    socket.on('close', function(data) {
-        console.log('Conexiunea: ' + socket.remoteAddress +' '+ socket.remotePort + ' s-a inchis.');
+       var mess = {};
+      	mess.sender = socket.name;
+      	mess.text = data.toString();
+     
+       	var json = JSON.stringify(mess);
+
+        fs.writeFile('messages.json', json, 'utf8');
+  
+        // scriem date pentru socket, clientul va primi datele ca date de la server
+        broadcast(socket.name + ": " + data +"\n", socket);
+        
     });
+
+    // Remove the client from the list when it leaves
+  	socket.on('end', function () {
+    	clients.splice(clients.indexOf(socket), 1);
+    	broadcast(socket.name + " left the chat.\n");
+  	});
+    
+    // Send a message to all clients
+  	function broadcast(message, sender) {
+    	clients.forEach(function (client) {
+     		// Don't want to send it to sender
+      		if (client === sender) return;
+      		client.write(message);
+    });
+    // Log it to the server output too
+    process.stdout.write(message)
+  }
+
+  socket.on('error', function(){
+  })
     
 }).listen(PORT, HOST);
 
